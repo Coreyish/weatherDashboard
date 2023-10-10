97d65ba71f5fa27441b58aef966aa01a003de2eb
Coreyish <corey.obiri@gmail.com>
2023-10-09 20:34:26 -0400

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,4 +7,15 @@ var searchHistoryList = function(cityName){
 //TODO:search for current weather section 
 var currentWeatherSection = function(cityName) {
     //method to fetch data for weather dashboard
-}
+    fetch('https')
+    // turn data into objects
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+    //method to get cities long and lat 
+    var cityLon = response.coord.lon;
+    var cityLat = response.coord.lat;
+
+    fetch('https')
+    })}
